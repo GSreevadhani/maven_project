1806912abb2c2f39352b59834025c9cb738e290b
G.SREEVADHANI <127740281+GSreevadhani@users.noreply.github.com>
2025-05-13 13:40:14 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,9 @@
-docker build -t maven-img -f Dockerfile .
+#!/bin/bash
+
+# Step 1: Build Docker image
+echo "Building Docker image..."
+docker build -t maven-java-app .
+
+# Step 2: Run the Docker container
+echo "Running Docker container..."
+docker run --rm -it maven-java-app
