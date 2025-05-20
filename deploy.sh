4addb21cb6996e76eb10ec6e284f1ad394db0fd6
G.SREEVADHANI <127740281+GSreevadhani@users.noreply.github.com>
2025-05-20 10:01:12 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 # Variables
-IMAGE_NAME="sreevadhani30/mave"
+# deploy.sh
+IMAGE_NAME="subiksha17/maven_project"
 LOCAL_TAG="test"
 
 # Step 1: Build Docker image
 docker build -t $LOCAL_TAG -f Dockerfile .
 
-# Step 2: Authenticate to Docker Hub
-echo "soundbetter" | docker login -u sreevadhani30 --password-stdin
+# Step 2: Authenticate to Docker Hub using token (not plain password)
+docker login -u subiksha17 -p dckr_pat_qdjWp1JpC1XIdg_SM_3ZONENje8
 
 # Step 3: Tag and push image to Docker Hub
 docker tag $LOCAL_TAG $IMAGE_NAME
 docker push $IMAGE_NAME
+
